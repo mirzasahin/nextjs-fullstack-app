eca4c2438d606813d9bedf4fbacf37d6e9a9c7b8
Mirza Sahin <salihmirza123@gmail.com>
2024-06-13 14:14:35 +0300

    Style home page features

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import styles from './footer.module.css'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div>© Vinhart. All rights reserverd.</div>
-      <div></div>
+      <div className={styles.social}>
+        <Image src="/1.png" width={15} height={15} className={styles.icon} alt='' />
+        <Image src="/2.png" width={15} height={15} className={styles.icon} alt='' />
+        <Image src="/3.png" width={15} height={15} className={styles.icon} alt='' />
+        <Image src="/4.png" width={15} height={15} className={styles.icon} alt='' />
+      </div>
     </div>
   )
 }
